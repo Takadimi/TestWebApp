aff14f23a47d82f66fe89bede691cfb89c5ea786
Takadimi <Ethan.Woodward@gmail.com>
2013-09-23 00:28:23 +0000

    Added index.html and moved index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,27 @@
 <?php
-
+    
 //	Controller
-echo 'Testing user access';
+
+	if ($_SERVER['REQUEST_URI'] != '/') {
+		
+		preg_match('!name/([a-z]+)!imsx', $_SERVER['REQUEST_URI'], $pmatches);
+		
+		$content = file_get_contents("../templates/index.html");
+
+		if ($pmatches[1] == 'FuckFace') {
+			$content = str_replace('{text}', 'Go Away, '.$pmatches[1], $content);
+		} else {
+			$content = str_replace('{text}', 'Hello, '.$pmatches[1], $content);	
+		}
+
+	echo $content;	
+
+		// Homepage
+
+	} else {
+
+		echo "You are not on the homepage!@!!";
+		// Not Homepage
+	}
+
+
